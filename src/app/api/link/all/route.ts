62f6a455ddc2baa84f7f20895e6874980c0bfffa
Uzaifm127 <uzaifm127@gmail.com>
2024-12-07 22:34:19 +0530

    added the headers in link/all in master

--- src/app/api/link/all/route.ts
+++ src/app/api/link/all/route.ts
@@ -1,14 +1,23 @@
 import { prisma } from "@/lib/prisma";
 import { NextResponse } from "next/server";
 
+const headers = {
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+  "Access-Control-Allow-Headers": "Content-Type, Authorization",
+};
+
 export const GET = async () => {
   try {
     const links = await prisma.link.findMany({ include: { tags: true } });
 
-    return NextResponse.json({ links });
+    return NextResponse.json({ links }, { headers });
 
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
   } catch (error: any) {
-    return NextResponse.json({ message: error.message }, { status: 400 });
+    return NextResponse.json(
+      { message: error.message },
+      { status: 400, headers }
+    );
   }
 };
